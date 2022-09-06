1399ecddfcce035fa2241da655c0939926e0c604
Invicybowl <unconfigured@null.spigotmc.org>
2022-09-06 14:25:27 -0400

    Initial commit

---
+++ src/main/java/me/invic/invicshuds/clientInitializer.java
@@ -0,0 +1,12 @@
+package me.invic.invicshuds;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class clientInitializer implements ClientModInitializer
+{
+    @Override
+    public void onInitializeClient()
+    {
+
+    }
+}
